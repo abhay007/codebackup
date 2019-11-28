975cf460fc9700ddc7658bda3664545536e6bca1
Abhay Singh <abhaykr.singh@hotmail.com>
2019-11-28 09:54:54 +0530

    Update code_backup_delete.sh

--- code_backup_delete.sh
+++ code_backup_delete.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+####################################
+#
+#Delete Code backup which is more then then 7 days old
+#
+####################################
 find /var/backup/code_backup -mtime +7 -exec rm -rf {} \;
 echo "Code backup deleted older then last 1 weeks  | All Server" | sendmail -v abhaykr.singh@hotmail.com
