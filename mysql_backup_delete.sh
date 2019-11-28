046208ffb24187ed256d6fb1fbd91848022f6947
Abhay Singh <abhaykr.singh@hotmail.com>
2019-11-28 09:54:11 +0530

    Update mysql_backup_delete.sh

--- mysql_backup_delete.sh
+++ mysql_backup_delete.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+####################################
+#
+#Delete MySql old Dump which is more then then 7 days old
+#
+####################################
 find /var/backup/mysql_backup/ -mtime +7 -exec rm -rf {} \;
 echo "MySQL dump deleted older then last 7 days  | Server1" | sendmail -v abhaykr.singh@hotmail.com 
